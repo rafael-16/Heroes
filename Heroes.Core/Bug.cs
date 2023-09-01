04b29dd0fe61dd59a61ae254dca59ee9d415a21f
Рафаэль Мухамедьяров <Rafael17IS16@gmail.com>
2023-09-01 17:46:13 +0300

    Add classes Hero & Bug

---
+++ Heroes.Core/Bug.cs
@@ -0,0 +1,8 @@
+﻿namespace Heroes.Core.Bugs;
+
+public abstract class Bug : Hero
+{
+    private const int HEALTH = 200;
+
+    protected Bug(string name) : base(HEALTH, name) { }
+}
---
+++ Heroes.Core/Hero.cs
@@ -0,0 +1,37 @@
+﻿namespace Heroes.Core;
+
+public abstract class Hero
+{
+    private readonly int maxHealth;
+
+    public string Name { get; init; }
+
+    private int health;
+
+    public int Health
+    {
+        get => this.health;
+        set
+        {
+            if (value < 0)
+            {
+                this.health = 0;
+            }
+            else if (value > this.maxHealth)
+            {
+                this.health = this.maxHealth;
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+    }
+
+    protected Hero(int health, string name)
+    {
+        this.health = health;
+        this.Name = name;
+        this.maxHealth = health;
+    }
+}
